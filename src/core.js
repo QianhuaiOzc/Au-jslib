8c25090e1041b0aba205f326b1f82f1c6979e22a
ClarenceAu <ozhencong@gmail.com>
2012-05-12 23:27:55 +0800

    add some structure methods

--- src/core.js
+++ src/core.js
@@ -2,7 +2,17 @@ var Au = (function() {
     
     var Au = function() {
 
-    };
+    },
+
+    eventUtil = {
+    	addHandler: function(element, type, handler) {
+    		
+    	},
+
+    	removeHandler: function(element, type, handler) {
+    		
+    	}
+	};
 
     return Au;
 })();
