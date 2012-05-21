94d83441028be657b328af6aa01441ae9c6c018e
ClarenceAu <ozhencong@gmail.com>
2012-05-21 23:40:13 +0800

    update

--- src/core.js
+++ src/core.js
@@ -1,5 +1,9 @@
 var Au = (function() {
-    
+    /*
+     *
+     * The structure of this lib is temporary
+     * 
+     */
     var Au = function() {
 
     },
@@ -12,7 +16,28 @@ var Au = (function() {
     	removeHandler: function(element, type, handler) {
     		
     	}
-	};
+	},
+
+    loadScript = function(url, callback) {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+
+        if(script.readyState) {
+            script.onreadystatechange = function() {
+                if(script.readyState === "loaded" || script.readyState === "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                } 
+            }; 
+        }  else {
+            script.onload = function() {
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    };
 
     return Au;
 })();
